ef678e08eeeda4be1e930a1981fe4a4b158485e8
iamdudebot <iamdudebot@gmail.com>
2016-11-15 20:01:53 +0000

    Update slack_bot.js

--- slack_bot.js
+++ slack_bot.js
@@ -1,53 +1,210 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-           ______     ______     ______   __  __     __     ______
-          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
-          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
-           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
-            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
+'use strict';
 
+var async = require('async');
+var escapeStringRegexp = require('escape-string-regexp');
+var botkit = require('botkit');
+var mongodb = require('mongodb');
+var today = new Date().getDate();
 
-This is a sample Slack bot built with Botkit.
+function connectToDb() {
+  mongodb.MongoClient.connect('mongodb://localhost:27017/talking', function(err, db) {
+    if (err) {
+      throw err;
+    }
+    console.log('Connection established to mongodb');
+    startBot(db);
+  });
+}
 
-This bot demonstrates many of the core features of Botkit:
+function startBot(db) {
+  var collection = db.collection('talking');
+  // collection.remove({});
 
-* Connect to Slack using the real time API
-* Receive messages based on "spoken" patterns
-* Reply to messages
-* Use the conversation system to ask questions
-* Use the built in storage system to store and retrieve information
-  for a user.
+  collection.updateMany({
+    lastChange: today - 1
+  }, {
+    $set: {
+      "score": 0
+    }
+  });
 
-# RUN THE BOT:
+  var botkitController = botkit.slackbot({
+    debug: false
+  });
 
-  Get a Bot token from Slack:
+  botkitController.spawn({
+    token: process.env.token
+  }).startRTM();
 
-    -> http://my.slack.com/services/new/bot
+  botkitController.on('ambient', function(bot, message, token) {
+    var talk = message.text.length;
 
-  Run your bot from the command line:
+    collection.findAndModify({
+      _id: message.user
+    }, [
+      ['_id', 1]
+    ], {
+      $inc: {
+        "score": talk
+      },
+      $set: {
+        lastChange: today
+      },
+      $setOnInsert: {
+        dudeStatus: null
+      }
+    }, {
+      new: true,
+      upsert: true
+    }, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      bot.botkit.log('individual message log', result);
+    });
 
-    token=<MY TOKEN> node slack_bot.js
+  });
 
-# USE THE BOT:
+  botkitController.hears(['report'], ['direct_message', 'direct_mention'], function(bot, message) {
+    bot.reply(message, "First, let's make sure everyone's dude status has been set.");
 
-  Find your bot inside Slack to send it a direct message.
+    function assignDude(status, el, convo) {
+      convo.on('end', function(convo) {
+        if (convo.status == 'completed') {
 
-  Say: "Hello"
+          collection.findAndModify({
+            _id: el._id
+          }, [
+            ['_id', 1]
+          ], {
+            $set: {
+              dudeStatus: status
+            }
+          }, {
+            new: true,
+            upsert: false
+          }, function(err, result) {
+            if (err) {
+              throw err;
+            }
+            //bot.botkit.log('individual dude log', result);
+            convo.stop();
+            talkedToday();
+          });
+        }
+      });
+    }
 
-  The bot will reply "Hello!"
+    function askDude(el, convo) {
+      convo.ask('Is ' + '<@' + el._id + '>' + ' a dude?', [
+        {
+          pattern: bot.utterances.yes,
+          callback: function(response, convo) {
+            assignDude(true, el, convo);
+            convo.say('OK! I will update my dossier for ' + '<@' + el._id + '>');
+            convo.next();
+          }
+        },
+        {
+          pattern: bot.utterances.no,
+          callback: function(response, convo) {
+            assignDude(false, el, convo);
+            convo.say('OK! I will update my dossier for ' + '<@' + el._id + '>');
+            convo.next();
+          }
+        },
+        {
+          default: true,
+          callback: function(response,convo) {
+            // just repeat the question
+            convo.say("No problem! I'll ask again and you can just say " + '<@' + el._id + '>' + ' is not a dude.');
+            convo.repeat();
+            convo.next();
+          }
+        }
+      ]);
+      convo.activate();
+    }
 
-  Say: "who are you?"
+    function getReport(callback) {
+      collection.aggregate([{
+        $match: { $or: [ { dudeStatus: true }, { dudeStatus: false } ] } },
+      {
+        $group: {
+          _id: {
+            "dudeStatus": "$dudeStatus"
+          },
+          "total": {
+            $sum: "$score"
+          }
+        }
+      }]).toArray(
+      function(err, result) {
+        if (err) {
+          throw err;
+        } else if (result.length === 2) {
+          bot.reply(message, "All dude statuses are in!");
 
-  The bot will tell you its name, where it is running, and for how long.
+          var dudeTalk = result[0].total;
+          var notDudeTalk = result[1].total;
+          var totalTalk = dudeTalk + notDudeTalk;
+          var percentageDude = Math.trunc((dudeTalk / totalTalk) * 100);
 
-  Say: "Call me <nickname>"
+          bot.botkit.log('report total log', totalTalk);
+          bot.botkit.log('report result log', result);
 
-  Tell the bot your nickname. Now you are friends.
+          bot.reply(message, 'Dudes have talked:\n' + percentageDude + '% of the time');
+        } else {
+          bot.botkit.log('report result error', result);
+          bot.reply(message, "I don't have enough information to move forward. 😿");
+        }
+      });
+    }
 
-  Say: "who am I?"
+    function talkedToday() {
+      collection.aggregate([{
+        $match: {
+          lastChange: today
+        }
+      }]).toArray(
+        function(err, result) {
+          if (err) {
+            throw err;
+          }
 
-  The bot will tell you your nickname, if it knows one for you.
+          bot.botkit.log('Talked Today log', result);
 
-  Say: "shutdown"
+          bot.startConversation(message, function(err, convo) {
+
+            async.eachSeries(result, function(el, cb) {
+              if (el.dudeStatus === null) {
+                askDude(el, convo);
+              } else {
+                cb();
+              }
+            }, function(err) {
+              if( err ) {
+                bot.reply(message, 'Something went wrong. Try again?');
+              } else {
+                getReport();
+              }
+            })
+          })
+        })
+    }
+
+    talkedToday();
+
+  });
+
+}
+
+if (!process.env.token) {
+  console.log('Error: Specify token in environment');
+  process.exit(1);
+}
+
+connectToDb();
 
   The bot will ask if you are sure, and then shut itself down.
 
